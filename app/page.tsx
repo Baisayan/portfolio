532c52f39cdbe7ce5b1020372c7f8fb20f26248c
Ved <bbbbaisayan@gmail.com>
2026-02-15 15:16:14 +0530

    added header and converted the site to dark theme through layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-// import Header from "@/components/Header";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return <main className="min-h-screen">
-    {/* <Header /> */}
+    <Header />
     <div className="max-w-4xl mx-auto px-6 py-12">
       <Hero />
     </div>
